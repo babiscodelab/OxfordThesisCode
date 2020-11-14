8148f183432220b64593b9fadb67e811f4e4591a
Babis Moustafelos <charalampos.moustafelos@d-fine.co.uk>
2020-11-14 16:18:20 +0100

    -- Piterbarg y_bar test added.

--- tests/fastcalibration/approximation.py
+++ tests/fastcalibration/approximation.py
@@ -1,6 +1,33 @@
+from quassigaussian.fastcalibration.approximation import PiterbargApproximator
+from quassigaussian.volatility.local_volatility import LinearLocalVolatility
+from quassigaussian.products.pricer import SwapPricer
+from quassigaussian.curves.libor import LiborCurve
+import numpy as np
+from scipy.interpolate import interp1d
 
 
+def test_piterbarg_y_bar():
 
-def test_piterbarg_x0():
-    pass
+    kappa = 0.2
+    x = np.arange(0, 31)
+    y = np.ones(31)
+    t = 0.5
+
+
+    lambda_t = interp1d(x, y, kind='previous')
+    alpha_t = interp1d(x, y, kind='previous')
+    b_t = interp1d(x, y, kind='previous')
+
+    tmp_file = r"C:\Users\d80084\Google Drive\01oxford\7 Thesis\code\quasigaussian\data\market_data\libor_curve\usd_libor\sofr_curve.csv"
+    initial_curve = LiborCurve.from_file(tmp_file, "2013-05-20")
+
+    swap_pricer = SwapPricer(initial_curve, kappa=kappa)
+
+    sigma_r = LinearLocalVolatility(lambda_t, alpha_t, b_t)
+    piterbarg_approx = PiterbargApproximator(sigma_r, swap_pricer)
+
+    y_bar_actual = piterbarg_approx._calculate_ybar(t)
+    y_bar_expected = np.exp(-2*kappa*t)*(np.exp(2*kappa*t) - 1)/(2*kappa)
+
+    np.testing.assert_approx_equal(y_bar_actual, y_bar_expected)
 
