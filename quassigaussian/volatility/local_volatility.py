db91d48877108cb0bed287ed93ac07fcb2ce58bf
Babis Moustafelos <charalampos.moustafelos@d-fine.co.uk>
2020-11-21 18:23:18 +0100

    -- Added functionalities in local volatility function

--- quassigaussian/volatility/local_volatility.py
+++ quassigaussian/volatility/local_volatility.py
@@ -1,6 +1,6 @@
-
-
+from scipy.interpolate import interp1d
 from abc import ABC, abstractmethod
+import numpy as np
 
 class LocalVolatility(ABC):
 
@@ -11,9 +11,24 @@ class LocalVolatility(ABC):
 class LinearLocalVolatility():
 
     def __init__(self, lambda_t: callable, alpha_t: callable, b_t: callable):
-        self.lamba_t = lambda_t
+        self.lambda_t = lambda_t
         self.alpha_t = alpha_t
         self.b_t = b_t
 
     def calculate_vola(self, t: float, x: float, y=0):
-        return self.lamba_t(t) * (self.alpha_t(t) + x * self.b_t(t))
+        return self.lambda_t(t) * (self.alpha_t(t) + x * self.b_t(t))
+
+    def d_vola_dx(self, t, x, y=0):
+        return self.lambda_t(t) * self.alpha_t(t)
+
+    @classmethod
+    def from_const(cls, maturity, lambda_const, alpha_const, b_const):
+
+        x = np.arange(0, maturity+1)
+        y = np.ones(maturity+1)
+
+        lambda_t = interp1d(x, y*lambda_const, kind='previous')
+        alpha_t = interp1d(x, y*alpha_const, kind='previous')
+        b_t = interp1d(x, y*b_const, kind='previous')
+
+        return cls(lambda_t, alpha_t, b_t)
