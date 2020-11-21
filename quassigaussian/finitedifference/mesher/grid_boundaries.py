212f46b8dd8d41fe3cb9eb4ffe0bd31548e268cd
Babis Moustafelos <charalampos.moustafelos@d-fine.co.uk>
2020-11-21 18:26:31 +0100

    -- calculate boundary grids in grid_boundaries.

--- quassigaussian/finitedifference/mesher/grid_boundaries.py
+++ quassigaussian/finitedifference/mesher/grid_boundaries.py
@@ -1,7 +1,10 @@
 import numpy as np
+from quassigaussian.volatility.local_volatility import LinearLocalVolatility
+import scipy.integrate as integrate
 
-def calculate_x_boundaries(y, kappa, maturity, eta_square):
+def calculate_x_boundaries(y, kappa, maturity, volatility: LinearLocalVolatility):
 
+    eta_square = volatility.calculate_vola(0, 0, 0)
     exp_x = y/kappa * (1-np.exp(-kappa*maturity))
     var_x = eta_square/(2*kappa) * (1-np.exp(-2*kappa*maturity))
 
@@ -11,6 +14,31 @@ def calculate_x_boundaries(y, kappa, maturity, eta_square):
     return x_min, x_max
 
 
+def calculate_x_boundaries2(maturity, volatility: LinearLocalVolatility, alpha=5):
+
+    x_max = alpha * volatility.calculate_vola(0, 0, 0) * np.sqrt(maturity)
+    x_min = -alpha * volatility.calculate_vola(0, 0, 0) * np.sqrt(maturity)
+
+    return x_min, x_max
+
+def calculate_y_boundaries(maturity, kappa, volatility: LinearLocalVolatility, alpha=5):
+
+    def exp_y_integral(u):
+        eta_square = np.power(volatility.calculate_vola(u, 0, 0), 2)
+        return eta_square * np.exp(-2*kappa*(maturity-u))
+
+    def var_y_integral(u):
+        return 4*np.power(volatility.d_vola_dx(u, 0, 0), 2) * np.power(volatility.calculate_vola(u, 0, 0), 2) * np.exp(-4*kappa*(maturity-u))
+
+    exp_y = integrate.quad(exp_y_integral, 0, maturity)[0]
+    var_y = integrate.quad(var_y_integral, 0, maturity)[0]
+
+    # y cannot be negative
+    y_min = 0
+    y_max = exp_y + alpha * np.sqrt(var_y)
+
+    return y_min, y_max
+
 
 if __name__ == "__main__":
 
@@ -19,4 +47,6 @@ if __name__ == "__main__":
     maturity = 1
     eta_square = 0.01**2
     print(np.sqrt(eta_square*maturity)*5)
-    print(calculate_x_boundaries(y, kappa, maturity, eta_square))
+
+    linear_local_vola = LinearLocalVolatility()
+    print(calculate_x_boundaries(y, kappa, maturity, ))
