8d475eb3ad730eb9864b425d5b47c80be66ea077
Babis Moustafelos <charalampos.moustafelos@d-fine.co.uk>
2020-12-02 23:57:56 +0100

    -- Added test function for ksi approximation.

--- qgtests/fastcalibration/approximation.py
+++ qgtests/fastcalibration/approximation.py
@@ -10,39 +10,50 @@ from scipy.interpolate import interp1d
 def test_piterbarg_y_bar():
 
     kappa = 0.001
-    x = np.arange(0, 31)
-    y = np.ones(31)*0.1
     t = 15
 
-
-    lambda_t = interp1d(x, y, kind='previous')
-    alpha_t = interp1d(x, y, kind='previous')
-    b_t = interp1d(x, y, kind='previous')
-
-    tmp_file = r"C:\Users\d80084\Google Drive\01oxford\7 Thesis\code\quasigaussian\data\market_data\libor_curve\usd_libor\sofr_curve.csv"
-    initial_curve = LiborCurve.from_file(tmp_file, "2013-05-20")
-
+    initial_curve = LiborCurve.from_constant_rate(0.06)
     swap_pricer = SwapPricer(initial_curve, kappa=kappa)
 
-    sigma_r = LinearLocalVolatility(lambda_t, alpha_t, b_t)
+    sigma_r = LinearLocalVolatility.from_const(t, 0.1, 0.1, 0)
     piterbarg_approx = PiterbargApproximator(sigma_r, swap_pricer)
 
     y_bar_actual = piterbarg_approx._calculate_ybar(t)
-    y_bar_expected = np.exp(-2*kappa*t)*(np.exp(2*kappa*t) - 1)/(2*kappa)
+    y_bar_expected = np.exp(-2*kappa*t)*(np.exp(2*kappa*t) - 1)/(2*kappa)*0.0001
 
-    #np.testing.assert_approx_equal(y_bar_actual, y_bar_expected)
+    np.testing.assert_approx_equal(y_bar_actual, y_bar_expected)
 
-    swap = Swap(29, 30, frequency=0.5)
 
-    swap_0 = piterbarg_approx.swap_pricer.price(swap, 0, 0, 0)
+def test_piterbarg_ksi():
 
-    x0 = piterbarg_approx._calculate_x0(t, swap, swap_0, y_bar_actual)
+    kappa = 0.001
+    t = 15
 
-    x_bar = piterbarg_approx._calculate_xbar(t, y_bar_actual, swap, swap_0)
+    initial_curve = LiborCurve.from_constant_rate(0.06)
+    swap_pricer = SwapPricer(initial_curve, kappa=kappa)
 
-    swap_price = swap_pricer.price(swap, x=0, y=0, t=t)
+    sigma_r = LinearLocalVolatility.from_const(t, 0.5, 0.5, 0.2)
 
-    x_simple = piterbarg_approx.x_bar_simple(t, swap_price, swap)
+    piterbarg_approx = PiterbargApproximator(sigma_r, swap_pricer)
+
+    swap = Swap(4, 5, 0.5)
+    swap_value = swap_pricer.price(swap, 0.01717964, 0.0025, 0.04)
+    ksi = piterbarg_approx.calculate_ksi(0.04, swap_value, swap)
+
     print("pause")
+
+    #
+    # swap = Swap(29, 30, frequency=0.5)
+    #
+    # swap_0 = piterbarg_approx.swap_pricer.price(swap, 0, 0, 0)
+    #
+    # x0 = piterbarg_approx._calculate_x0(t, swap, swap_0, y_bar_actual)
+    #
+    # x_bar = piterbarg_approx._calculate_xbar(t, y_bar_actual, swap, swap_0)
+    #
+    # swap_price = swap_pricer.price(swap, x=0, y=0, t=t)
+    #
+    # x_simple = piterbarg_approx.x_bar_simple(t, swap_price, swap)
+    # print("pause")
 
 test_piterbarg_y_bar()
