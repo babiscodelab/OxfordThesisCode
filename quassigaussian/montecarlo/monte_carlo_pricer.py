ca4a908273c03c4c86028d7ddbc976621e0ce3e3
Babis Moustafelos <charalampos.moustafelos@d-fine.co.uk>
2020-12-23 20:01:03 +0100

    -- Solved bug with MC pricing in Swaption under Annuity measure.

--- quassigaussian/montecarlo/monte_carlo_pricer.py
+++ quassigaussian/montecarlo/monte_carlo_pricer.py
@@ -4,10 +4,14 @@ from quassigaussian.products.pricer import SwaptionPricer
 
 
 def monte_carlo_pricer(res: ResultSimulatorObj, swaption: Swaption, swaption_pricer: SwaptionPricer):
+    """
+    Price under Annuity measure
+    """
 
     x = res.x[:, -1]
     y = res.y[:, -1]
 
     annuity_texp = swaption_pricer.swap_pricer.annuity_pricer.annuity_price(swaption.expiry, x, y, swaption.swap.annuity)
-    annuity_t0 = swaption_pricer.swap_pricer.annuity_pricer.annuity_price(0, x, y, swaption.swap.annuity)
-    return swaption_pricer.maturity_price(swaption, x, y)/annuity_texp * annuity_t0
+    annuity_t0 = swaption_pricer.swap_pricer.annuity_pricer.annuity_price(0, 0, 0, swaption.swap.annuity)
+    result = swaption_pricer.maturity_price(swaption, x, y)/annuity_texp * annuity_t0
+    return result
