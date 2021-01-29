473a6bd81af2685ee44df98d3e1e80eca5cb641f
Babis Moustafelos <charalampos.moustafelos@d-fine.co.uk>
2021-01-29 11:25:12 +0100

    -- Annuity as control variate.

--- quassigaussian/montecarlo/control_variate.py
+++ quassigaussian/montecarlo/control_variate.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-def apply_control_variate(x, y, mc_simulation_product, bond, bond_pricer):
+def apply_control_variate(last_time, x, y, mc_simulation_product, bond, bond_pricer, initial_curve):
 
-    mc_simulation_bond = bond_pricer.price(bond, x, y, 0)
+    mc_simulation_bond = initial_curve.get_discount(last_time)*bond_pricer.price(bond, x, y, last_time)
 
     beta = calculate_beta(mc_simulation_bond, mc_simulation_product)
     bond_value = bond_pricer.price(bond, 0, 0, 0)
@@ -11,6 +11,17 @@ def apply_control_variate(x, y, mc_simulation_product, bond, bond_pricer):
 
     return res
 
+
+def apply_control_variate_annuity(last_time, x, y, mc_simulation_product, annuity, annuity_pricer, initial_curve):
+
+    mc_simulation_bond = initial_curve.get_discount(last_time)*annuity_pricer.annuity_price(last_time, x, y, annuity)
+
+    beta = calculate_beta(mc_simulation_bond, mc_simulation_product)
+    bond_value = annuity_pricer.annuity_price(0, 0, 0, annuity)
+
+    res = mc_simulation_product - beta*(mc_simulation_bond - bond_value)
+
+    return res
 
 def calculate_beta(mc_simulation_bond, mc_simulation_product):
 
